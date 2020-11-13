3acf94b48de540010c4b94f2fe872ffc2315efa0
Administrator <Administrator@DESKTOP-N5VR3R3>
2020-11-13 09:46:47 +0800

    try2

---
+++ mvspringhibernate/src/main/java/com/Controller/EmpController.java
@@ -0,0 +1,48 @@
+package com.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import model.MyEmployee;
+
+@Controller
+public class EmpController {
+	static List<MyEmployee> list = new ArrayList<MyEmployee>();
+	static {
+		list.add(new MyEmployee(1, "rahul", 35000f, "S.Engineer"));
+		list.add(new MyEmployee(2, "aditya", 25000f, "IT Manager"));
+		list.add(new MyEmployee(3, "sachin", 55000f, "Care Taker"));
+	}
+
+	@RequestMapping("/empform")
+	public ModelAndView showform() {
+		// command is a reserved request attribute name, now use <form> tag to show
+		// object data
+		return new ModelAndView("empform", "command", new MyEmployee());
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute("emp") MyEmployee emp) {
+		// write code to save emp object
+		// here, we are displaying emp object to prove emp has data
+		list.add(new MyEmployee(list.size() + 1, emp.getName(), emp.getSalary(), emp.getDesignation()));
+		System.out.println(emp.getName() + " " + emp.getSalary() + " " + emp.getDesignation());
+
+		// return new ModelAndView("empform","command",emp);//will display object data
+		return new ModelAndView("redirect:/viewemp");// will redirect to viewemp request mapping
+	}
+
+	@RequestMapping("/viewemp")
+	public ModelAndView viewemp() {
+		// write the code to get all employees from DAO
+		// here, we are writing manual code of list for easy understanding
+
+		return new ModelAndView("viewemp", "list", list);
+	}
+}
+
+
---
+++ mvspringhibernate/src/main/java/com/Controller/UserTableController.java
@@ -0,0 +1,71 @@
+package com.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import model.UserTable;
+import org.hibernate.Session;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+@Controller
+public class UserTableController {
+    List<UserTable> list=new ArrayList<>();
+    @RequestMapping("/userform")
+    public ModelAndView showform() {
+        //command is a reserved request attribute name, now use <form> tag to show object data  
+        return new ModelAndView("userform", "command", new UserTable());
+    }
+
+    @RequestMapping(value = "/saveuser", method = RequestMethod.POST)
+    public ModelAndView save(@ModelAttribute("user") UserTable  user) {        
+        save2Hibernate(user);
+        list.add(user);
+        return new ModelAndView("redirect:/viewuser");//will redirect to viewemp request mapping  
+    }
+    public void save2Hibernate(UserTable u){
+        Configuration configObj = new Configuration();
+        configObj.addClass(model.UserTable.class);
+        configObj.configure("/model/hibernate.cfg.xml");
+ 
+        // Since Hibernate Version 4.x, ServiceRegistry Is Being Used
+        ServiceRegistry serviceRegistryObj = new StandardServiceRegistryBuilder().applySettings(configObj.getProperties()).build(); 
+          Session  sessionObj=null;
+        // Creating Hibernate SessionFactory Instance
+           try {
+                sessionObj = configObj.buildSessionFactory(serviceRegistryObj).openSession();
+                sessionObj.beginTransaction();
+                sessionObj.save(u);           
+                System.out.println("\n.......Records Saved Successfully To The Database.......\n");
+ 
+             // Committing The Transactions To The Database
+             sessionObj.getTransaction().commit();
+                
+           } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                System.out.println("\n.......Transaction Is Being Rolled Back.......");
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+       
+    }
+  
+    @RequestMapping("/viewuser")
+    public ModelAndView viewemp() {
+        //write the code to get all employees from DAO  
+        //here, we are writing manual code of list for easy understanding      
+
+        return new ModelAndView("viewuser", "list", list);
+    }
+}
+
